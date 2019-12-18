5e8c4f8cf5f7169e7f23264bc9ff343b45df0c6c
Ostrowski_Bartosz <bartosz.ostrowski123@gmail.com>
2019-12-18 23:35:32 +0100

    Updates

---
+++ precache-manifest.e99741c1b0e3bafaffa618a07f7cb4c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0695175c47fca1f909bd7c497063e641",
+    "url": "/ideal-pancake/index.html"
+  },
+  {
+    "revision": "8427212d7220927bf84d",
+    "url": "/ideal-pancake/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9f71d866d92d1f9b1d0e",
+    "url": "/ideal-pancake/static/js/2.f2fe1928.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ideal-pancake/static/js/2.f2fe1928.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8427212d7220927bf84d",
+    "url": "/ideal-pancake/static/js/main.c070d7e2.chunk.js"
+  },
+  {
+    "revision": "b513b13e7f56067ae1db",
+    "url": "/ideal-pancake/static/js/runtime-main.9448ee2f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ideal-pancake/static/media/logo.5d5d9eef.svg"
+  }
+]);
